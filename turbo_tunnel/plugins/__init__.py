932295f67d017ec0e4d241aee4868fecf4a3d5c2
drunkdream <drunkdream@qq.com>
2020-04-21 13:36:25 +0800

    Add terminal plugin

---
+++ turbo_tunnel/plugins/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+'''Internal plugins
+'''
+
+
+class Plugin(object):
+
+    def on_load(self):
+        pass
+
+    def on_unload(self):
+        pass
+
+    def on_new_connection(self, connection):
+        pass
+
+    def on_data_recevied(self, connection, buffer):
+        pass
+
+    def on_data_sent(self, connection, buffer):
+        pass
+
+    def on_connection_closed(self, connection):
+        pass
