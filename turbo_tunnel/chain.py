75d28a38ae8c9a38c04f81e4a92a56eb2ab2b61f
drunkdream <drunkdream@qq.com>
2020-05-10 08:14:04 +0800

    Support cached tunnel

--- turbo_tunnel/chain.py
+++ turbo_tunnel/chain.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 '''Tunnel Chain
 '''
 
@@ -16,7 +15,6 @@ from . import utils
 class TunnelChain(object):
     '''Tunnel Chain
     '''
-
     def __init__(self, tunnel_router_or_urls, try_connect_count=1):
         self._tunnel_router = self._tunnel_urls = None
         if isinstance(tunnel_router_or_urls, route.TunnelRouter):
@@ -59,6 +57,16 @@ class TunnelChain(object):
 
         return func_wrapper
 
+    def get_cached_tunnel(self):
+        for i, url in enumerate(self._tunnel_urls[::-1]):
+            tunnel_class = registry.tunnel_registry[url.protocol]
+            if not tunnel_class:
+                raise utils.TunnelError('%s tunnel not registered' %
+                                        url.protocol.upper())
+            if tunnel_class.has_cache(url):
+                return len(self._tunnel_urls) - i - 1
+        return -1
+
     async def create_tunnel(self, address):
         if self._tunnel_router:
             selected_tunnel = self._tunnel_router.select(address)
@@ -75,22 +83,30 @@ class TunnelChain(object):
                 (self.__class__.__name__, ', '.join(
                     [str(url)
                      for url in self._tunnel_urls]), address[0], address[1]))
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-        tunnel_stream = tornado.iostream.IOStream(s)
-        tun = utils.TCPStream(tunnel_stream)
-        self._tunnel_list.append(tun)
+
         tunnel_address = address
         if self._tunnel_urls and self._tunnel_urls[
                 0].host and self._tunnel_urls[0].port:
             tunnel_address = (self._tunnel_urls[0].host,
                               self._tunnel_urls[0].port)
 
-        try:
-            await tunnel_stream.connect(tunnel_address)
-        except tornado.iostream.StreamClosedError:
-            raise utils.TunnelConnectError('Connect %s:%d failed' % address)
+        tunnel_urls = self._tunnel_urls[:]
+        cached_tunnel_index = self.get_cached_tunnel()
+        if cached_tunnel_index < 0:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+            stream = tornado.iostream.IOStream(s)
+            tunn = tunnel.TCPTunnel(stream, None, tunnel_address)
+            await tunn.connect()
+            self._tunnel_list.append(tunn)
+        else:
+            utils.logger.info(
+                '[%s] Found cached tunnel %s to %s:%d' %
+                (self.__class__.__name__, tunnel_urls[cached_tunnel_index],
+                 address[0], address[1]))
+            tunnel_urls = tunnel_urls[cached_tunnel_index:]
+            tunn = None
 
-        for i, url in enumerate(self._tunnel_urls):
+        for i, url in enumerate(tunnel_urls):
             tunnel_class = registry.tunnel_registry[url.protocol]
             if not tunnel_class:
                 raise utils.TunnelError('%s tunnel not registered' %
@@ -99,20 +115,21 @@ class TunnelChain(object):
             if i < len(self._tunnel_urls) - 1:
                 next_url = self._tunnel_urls[i + 1]
                 next_address = next_url.host, next_url.port
-            tun = tunnel_class(tun, url, next_address)
-            self._tunnel_list.append(tun)
+            tunn = tunnel_class(tunn, url, next_address)
+            self._tunnel_list.append(tunn)
 
-            if not await tun.connect():
-                raise utils.TunnelConnectError('Connect %s failed' % tun)
+            if not await tunn.connect():
+                raise utils.TunnelConnectError('Connect %s failed' % tunn)
             utils.logger.debug('[%s] Tunnel to %s established' %
                                (self.__class__.__name__, url))
         utils.logger.info('[%s] Create tunnel to %s:%d success' %
                           (self.__class__.__name__, address[0], address[1]))
 
     def close(self):
-        for tunnel in self._tunnel_list:
+        tunnel = self.tail
+        if tunnel:
             tunnel.close()
-        self._tunnel_list = []
+            self._tunnel_list = []
 
     def __enter__(self):
         return self
