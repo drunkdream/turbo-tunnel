1896b998c67da3e510743c555e4470072b9b8613
drunkdream <drunkdream@qq.com>
2020-10-18 16:59:01 +0800

    Add ssl tunnel test

--- tests/test_tunnel.py
+++ tests/test_tunnel.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import os
 import random
 import socket
+import ssl
 
 import pytest
 
-from turbo_tunnel import tunnel
+from turbo_tunnel import tunnel, utils
 
 from .util import DemoTCPServer, get_random_port
 
@@ -16,9 +18,31 @@ async def test_tcp_tunnel():
     port = get_random_port()
     server.listen(port)
     s = socket.socket()
-    tunn = tunnel.TCPTunnel(s, address=('127.0.0.1', port))
+    tunn = tunnel.TCPTunnel(s, address=("127.0.0.1", port))
     await tunn.connect()
-    data = b'Hello world\n'
+    data = b"Hello world\n"
+    await tunn.write(data)
+    buffer = await tunn.read()
+    assert buffer == data
+    server.stop()
+
+
+@pytest.mark.asyncio
+async def test_ssl_tunnel():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    ssl_ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ssl_ctx.load_cert_chain(
+        os.path.join(current_dir, "test.crt"), os.path.join(current_dir, "test.key")
+    )
+    server = DemoTCPServer(ssl_options=ssl_ctx)
+    port = get_random_port()
+    server.listen(port)
+    s = socket.socket()
+    tunn = tunnel.TCPTunnel(s, address=("127.0.0.1", port))
+    await tunn.connect()
+    tunn = tunnel.SSLTunnel(tunn, utils.Url("ssl://127.0.0.1/?verify_ssl=false"))
+    await tunn.connect()
+    data = b"Hello world\n"
     await tunn.write(data)
     buffer = await tunn.read()
     assert buffer == data
