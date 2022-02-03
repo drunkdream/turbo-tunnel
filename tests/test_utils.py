14ef36ad9b766879506f54c64ca502025ab16b17
drunkdream <drunkdream@qq.com>
2022-02-03 20:40:36 +0800

    Update tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,10 +3,9 @@
 """
 
 import asyncio
-import random
+import re
 import socket
-
-import tornado
+import time
 
 from turbo_tunnel import utils
 
@@ -65,3 +64,25 @@ async def test_wait_for_tasks():
     curr_tasks = len(async_task_mgr.running_tasks)
     await async_task_mgr.wait_for_tasks([task1(), task2()])
     assert len(async_task_mgr.running_tasks) == curr_tasks
+
+
+async def test_resolve_address():
+    orig_getaddrinfo = socket.getaddrinfo
+
+    def hooked_getaddrinfo(host, port, family=0, type=0, proto=0, flags=0):
+        raise RuntimeError("socket.getaddrinfo should not called")
+
+    socket.getaddrinfo = hooked_getaddrinfo
+
+    async def resolve(domain, expect_timeout=0):
+        time0 = time.time()
+        address = await utils.resolve_address((domain, 80))
+        time1 = time.time()
+        if expect_timeout > 0:
+            assert time1 - time0 <= expect_timeout
+        return address[0]
+
+    result = await resolve("www.qq.com", 0.1)
+    assert re.match(r"[\d\.]+", result)
+    result = await resolve("domainnotexist.com", 6)
+    assert result == "domainnotexist.com"
