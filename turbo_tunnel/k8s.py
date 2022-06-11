5f55c609ef51c80805956817953393822e2a62a2
drunkdream <drunkdream@qq.com>
2022-06-11 23:31:44 +0800

    Support kubeconfig

--- turbo_tunnel/k8s.py
+++ turbo_tunnel/k8s.py
@@ -3,9 +3,12 @@
 """Kubernetes Tunnel
 """
 
-
+import base64
 import os
+import tempfile
 import time
+
+import yaml
 
 from . import registry
 from . import websocket
@@ -27,7 +30,10 @@ class KubernetesTunnel(websocket.WebSocketTunnel):
         elif kubeconfig:
             if not os.path.isfile(kubeconfig):
                 raise ValueError("Kube config file %s not exist" % kubeconfig)
-            raise NotImplementedError(kubeconfig)
+            kubeconfig_data = self.handle_kebeconfig(kubeconfig)
+            ca_cert = kubeconfig_data["ca_cert"]
+            client_cert = kubeconfig_data["client_cert"]
+            client_key = kubeconfig_data["client_key"]
         else:
             if not os.path.isfile(ca_cert):
                 raise ValueError("CA cert file %s not exist" % ca_cert)
@@ -59,6 +65,69 @@ class KubernetesTunnel(websocket.WebSocketTunnel):
         )
         super(KubernetesTunnel, self).__init__(tunnel, utils.Url(ws_url), address)
 
+    def handle_kebeconfig(self, kubeconfig_path):
+        save_dir = tempfile.mkdtemp("kubeconfig")
+        ca_cert_path = client_cert_path = client_key_path = None
+        with open(kubeconfig_path) as fp:
+            kubeconfig = yaml.safe_load(fp.read())
+            if not kubeconfig.get("clusters"):
+                raise ValueError("Invalid `clusters` config in %s" % kubeconfig_path)
+            cluster = kubeconfig["clusters"][0].get("cluster")
+            if not cluster:
+                raise ValueError("Invalid `cluster` config in %s" % kubeconfig_path)
+            ca_cert_path = cluster.get("certificate-authority")
+            if not ca_cert_path:
+                ca_cert_data = cluster.get("certificate-authority-data")
+                if ca_cert_data:
+                    ca_cert_path = os.path.join(save_dir, "ca.crt")
+                    with open(ca_cert_path, "w") as fp:
+                        fp.write(base64.b64decode(ca_cert_data).decode())
+                else:
+                    raise ValueError("CA cert not found in %s" % kubeconfig_path)
+            else:
+                ca_cert_path = os.path.abspath(ca_cert_path)
+                if not os.path.isfile(ca_cert_path):
+                    raise RuntimeError("CA cert file %s not found" % ca_cert_path)
+
+            if not kubeconfig.get("users"):
+                raise ValueError("Invalid `users` config in %s" % kubeconfig_path)
+            user = kubeconfig["users"][0].get("user")
+            if not user:
+                raise ValueError("Invalid `user` config in %s" % kubeconfig_path)
+            client_cert_path = user.get("client-certificate")
+            if not client_cert_path:
+                client_cert_data = user.get("client-certificate-data")
+                if client_cert_data:
+                    client_cert_path = os.path.join(save_dir, "client.crt")
+                    with open(client_cert_path, "w") as fp:
+                        fp.write(base64.b64decode(client_cert_data).decode())
+                else:
+                    raise ValueError("Client cert not found in %s" % kubeconfig_path)
+            else:
+                client_cert_path = os.path.abspath(client_cert_path)
+                if not os.path.isfile(client_cert_path):
+                    raise RuntimeError("Client cert file %s not found" % client_cert_path)
+
+            client_key_path = user.get("client-key")
+            if not client_key_path:
+                client_key_data = user.get("client-key-data")
+                if client_key_data:
+                    client_key_path = os.path.join(save_dir, "client.key")
+                    with open(client_key_path, "w") as fp:
+                        fp.write(base64.b64decode(client_key_data).decode())
+                else:
+                    raise ValueError("Client key not found in %s" % kubeconfig_path)
+            else:
+                client_key_path = os.path.abspath(client_key_path)
+                if not os.path.isfile(client_key_path):
+                    raise RuntimeError("Client key file %s not found" % client_key_path)
+
+        return {
+            "ca_cert": ca_cert_path,
+            "client_cert": client_cert_path,
+            "client_key": client_key_path,
+        }
+
     async def _wait_for_connecting(self, timeout=15):
         time0 = time.time()
         while time.time() - time0 < timeout:
@@ -87,6 +156,12 @@ class KubernetesTunnel(websocket.WebSocketTunnel):
                     )
                 )
                 return False
+            elif stderr:
+                utils.logger.warning(
+                    "[%s] Stderr: %s" % (self.__class__.__name__, stderr.decode())
+                )
+                if self.closed():
+                    return False
         else:
             utils.logger.warning(
                 "[%s] Wait for connecting timeout" % self.__class__.__name__
@@ -109,6 +184,9 @@ class KubernetesTunnel(websocket.WebSocketTunnel):
             if buffer[0] == 1:
                 return buffer[1:], b""
             elif buffer[0] == 2:
+                return b"", buffer[1:]
+            elif buffer[0] == 3:
+                self.close()
                 return b"", buffer[1:]
             else:
                 raise NotImplementedError("Unsupported output: %r" % buffer)
