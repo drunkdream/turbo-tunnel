f352dc16a7cecbe9042364f33811ccc392644176
drunkdream <drunkdream@qq.com>
2022-06-15 00:33:10 +0800

    Update

--- tests/test_icmp.py
+++ tests/test_icmp.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import asyncio
 import os
 import sys
 
@@ -7,6 +8,21 @@ from turbo_tunnel import icmp
 from turbo_tunnel import utils
 
 disable_ping_file = "/proc/sys/net/ipv4/icmp_echo_ignore_all"
+
+
+def exec_command(cmdline, sync=True, raise_for_error=True):
+    proc = os.popen(cmdline)
+    if not sync:
+        return proc
+    stdout = proc.read()
+    exit_code = proc.close()
+    if exit_code:
+        err_msg = "Exec command `%s` failed: %d" % (cmdline, exit_code)
+        if raise_for_error:
+            raise RuntimeError(err_msg)
+        else:
+            print(err_msg, file=sys.stderr)
+    return stdout
 
 
 def root_required(func):
@@ -30,14 +46,29 @@ def setup_module(module):
     with open(disable_ping_file, "w") as fp:
         fp.write("1")
 
+    print(exec_command("ip link delete veth1", raise_for_error=False))
+    print(exec_command("ip netns delete netns1", raise_for_error=False))
+    print(exec_command("ip netns add netns1"))
+    print(exec_command("ip netns exec netns1 ip link set lo up"))
+    print(exec_command("ip netns exec netns1 sh -c 'echo 1 > %s'" % disable_ping_file))
+    print(exec_command("ip link add veth1 type veth peer name veth2"))
+    print(exec_command("ip link set veth2 netns netns1"))
+    print(exec_command("ip netns exec netns1 ip link set veth2 up"))
+    print(exec_command("ip netns exec netns1 ip addr add 192.168.100.2/24 dev veth2"))
+    print(exec_command("ip link set veth1 up"))
+    print(exec_command("ip addr add 192.168.100.1/24 dev veth1"))
+
 
 @root_required
 def teardown_module(module):
     if sys.platform != "linux":
         print("Ignore module teardown")
         return
+
     with open(disable_ping_file, "w") as fp:
         fp.write("0")
+    print(exec_command("ip link delete veth1"))
+    print(exec_command("ip netns delete netns1"))
 
 
 @root_required
@@ -53,6 +84,41 @@ async def test_icmp_socket():
     server_sock.sendto(icmp_packet.data * 2, address)
     _, icmp_packet = await client_sock.recvfrom(address)
     assert icmp_packet.data == buffer * 2
+
+
+@root_required
+async def test_icmp_tunnel():
+    event = asyncio.Event()
+
+    async def async_run_process(cmdline):
+        import signal
+
+        proc = await asyncio.create_subprocess_shell(cmdline, preexec_fn=os.setsid)
+        await event.wait()
+        os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
+        print("Process `%s` is killed" % cmdline)
+
+    asyncio.ensure_future(
+        async_run_process(
+            "ip netns exec netns1 %s -m http.server 8888" % sys.executable
+        )
+    )
+    asyncio.ensure_future(
+        async_run_process(
+            "ip netns exec netns1 %s -m turbo_tunnel -l icmp://192.168.100.2:8000/ --log-level verbose"
+            % sys.executable
+        )
+    )
+    await asyncio.sleep(2)
+
+    tunn = icmp.ICMPTunnel(
+        None, utils.Url("icmp://192.168.100.2:8000/"), ("192.168.100.2", 8888)
+    )
+    assert await tunn.connect()
+    await tunn.write(b"GET / HTTP/1.1\r\n\r\n")
+    response = (await tunn.read()).decode()
+    assert response.startswith("HTTP/1.0 200 OK\r\n")
+    event.set()
 
 
 class TestStreamHandler(icmp.ICMPTunnelStreamHandler):
