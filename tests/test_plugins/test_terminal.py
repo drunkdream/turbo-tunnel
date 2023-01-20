03c74d3475e956953b5f3b70f2796821fae1b6ce
drunkdream <drunkdream@qq.com>
2023-01-21 00:31:46 +0800

    Update

--- tests/test_plugins/test_terminal.py
+++ tests/test_plugins/test_terminal.py
@@ -3,13 +3,18 @@
 import os
 import sys
 
-from turbo_tunnel.plugins import terminal
+try:
+    from turbo_tunnel.plugins import terminal
+except:
+    terminal = None
 
 
 def test_terminal_view():
-    if sys.platform == "win32" or os.environ.get("TERM", "unknown") in ("unknown", "dumb"):
+    if terminal is None or os.environ.get("TERM", "unknown") in ("unknown", "dumb"):
         return
     screen = terminal.TerminalScreen()
     view = screen.create_view(screen.width, 15, (0, screen.height - 15))
     for i in range(14):
-        view.write_buffer("\x1b[%dm%s\x1b[0m<===\n" % (30+i, str(i)*50), refresh=True)
+        view.write_buffer(
+            "\x1b[%dm%s\x1b[0m<===\n" % (30 + i, str(i) * 50), refresh=True
+        )
